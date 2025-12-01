77382e22d9114983553fc55c25e7325c434ec870
bitflicker64 <himnshuverma10152006@gmail.com>
2025-12-02 00:40:15 +0530

    ig

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let board ;
-let bw=380;
-let bh= 800;
-let context;
-window.onload = function(){
+let board;
+let bw = 380;
+let bh = 800;
+let ctx;
+
+let ashx = 90;
+let ashy = 400;
+let ashwidth = 20;
+let ashheight = 30;
+let ash = {
+    x: ashx,
+    y: ashy,
+    hh: ashheight,
+    ww: ashwidth,
+};
+
+window.onload = function () {
     board = document.getElementById("board");
-    board.height=bh;
-    board.width=bw;
-    context = board.getContext("2d");
-}
+    board.height = bh;
+    board.width = bw;
+
+    ctx = board.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(ash.x, ash.y, ash.ww, ash.hh);
+};
