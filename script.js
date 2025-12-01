37da2ba96561df7f3d6cbb60450971e7f55fcb94
bitflicker64 <himnshuverma10152006@gmail.com>
2025-12-01 23:51:57 +0530

    ig

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let x;
-let y;
+let board ;
+let bw=380;
+let bh= 800;
+let context;
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height=bh;
+    board.width=bw;
+    context = board.getContext("2d");
+}
