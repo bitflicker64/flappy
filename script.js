f57d0ac847d725ca2473e4010efc60d0a8cfcc85
bitflicker64 <himnshuverma10152006@gmail.com>
2025-12-02 01:36:21 +0530

    added files and learned about onload , making a image object and also learned functions related to canvas lol

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+// 380 800 90 4000
 let board;
 let bw = 380;
 let bh = 800;
 let ctx;
-
 let ashx = 90;
-let ashy = 400;
-let ashwidth = 20;
-let ashheight = 30;
+let ashy = 400; 
+let ashwidth = 50;
+let ashheight = 50;
+
 let ash = {
-    x: ashx,
-    y: ashy,
-    hh: ashheight,
-    ww: ashwidth,
+    x: ashx , 
+    y: ashy ,
+    hh: ashwidth,
+    ww: ashheight,
 };
 
-window.onload = function () {
-    board = document.getElementById("board");
-    board.height = bh;
-    board.width = bw;
-
-    ctx = board.getContext("2d");
-    ctx.fillStyle = "green";
-    ctx.fillRect(ash.x, ash.y, ash.ww, ash.hh);
-};
+window.onload = function() {
+    board= document.getElementById("board");
+    board.height=bh; 
+    board.width=bw;
+    ctx=board.getContext("2d");
+    ctx.fillRect(ash.x,ash.y,ash.hh,ash.ww)
+    const ashimage = new Image();
+    ashimage.src ="./ash101.png"
+    ctx.drawImage(ashimage,ash.hh,ash.ww);
+        ashimage.onload = function() {
+        ctx.drawImage(ashimage, ash.x, ash.y, ash.ww, ash.hh);
+    }
+}
